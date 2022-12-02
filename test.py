9b591d7a68ca3194bb9ece96a66821a4432b71ca
olofer <k.e.j.olofsson@gmail.com>
2022-12-02 14:07:38 +0100

    add profiling demo to test.py incl. plot generation

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 #
 # Basic (smoke) tests of the rempc package
 #
+# Basic profiling demo like so:
+#   python3 test.py --profile-solver --tripleint -horizon 250
+#
 
 import argparse
 import numpy as np
@@ -12,9 +15,17 @@ if __name__ == '__main__':
   parser.add_argument('--smoke', action = 'store_true') 
   parser.add_argument('--tripleint', action = 'store_true') 
   parser.add_argument('--horizon', type = int, default = 200) 
-  # parser.add_argument('--profile-solver', type = int, default = -1) 
+  parser.add_argument('--profile-solver', action = 'store_true') 
+  parser.add_argument('--figext', type = str, default = 'pdf', help = 'figure file extension (e.g. pdf or png)')
+  parser.add_argument('--dpi', type = float, default = 250.0, help = 'print to file PNG option')
 
   args = parser.parse_args()
+
+  if args.tripleint:
+    from scipy.signal import cont2discrete
+
+  if args.profile_solver and args.tripleint:
+    import matplotlib.pyplot as plt
 
   if args.smoke:
     P = {}
@@ -50,8 +61,6 @@ if __name__ == '__main__':
     assert R['isconverged'] == 1
 
   if args.tripleint:
-    from scipy.signal import cont2discrete
-
     Ac = np.array([[0, 1, 0], [0, 0, 1], [0, 0, 0]], dtype = float)
     Bc = np.array([[0], [0] ,[1]], dtype = float)
     Cc = np.array([[1, 0, 0]], dtype = float)
@@ -89,7 +98,34 @@ if __name__ == '__main__':
     print(R.keys())
     assert R['isconverged'] == 1
 
-    #if args.profile_solver >= 5:
-    #  None
+    if args.profile_solver:
+      O['verbosity'] = int(0)
+      numRepeats = 20
+      nvec = np.arange(2, args.horizon + 1)
+      clocks = np.tile(np.nan, (len(nvec), 2))
+      print('profiling solver performance vs horizon length n={}..{}'.format(nvec[0], nvec[-1]))
+      for k in range(len(nvec)):
+        nk = int(nvec[k])
+        P['n'] = nk 
+        Rk = [rempc.qpmpclti2f(P, O) for _ in range(numRepeats)]
+        all_good = [rk['isconverged'] for rk in Rk]
+        assert np.sum(all_good) == numRepeats
+        idx = np.argmin([rk['totalclock'] for rk in Rk])
+        clocks[k, 0] = [rk['totalclock'] for rk in Rk][idx]
+        clocks[k, 1] = [rk['solveclock'] for rk in Rk][idx]
+      
+      plt.plot(nvec, 1.0e3 * clocks[:, 0], label = 'total (solve + overhead)')
+      plt.plot(nvec, 1.0e3 * clocks[:, 1], label = 'solve only')
+      plt.xlabel('length of horizon')
+      plt.ylabel('wall clock [ms] (best of {} repeats)'.format(numRepeats))
+      plt.grid(True)
+      plt.title('MPC solver profiling (nx={}, nu={}, ny={}, ni={})'.format(P['A'].shape[0], 
+                                                                           P['B'].shape[1], 
+                                                                           P['C'].shape[0], 
+                                                                           P['f3'].size))
+      plt.legend()
+      plt.tight_layout()
+      plt.savefig('test-profile-{}.{}'.format(args.horizon, args.figext), dpi = args.dpi)
+      plt.close()
 
   print('reached end of {}'.format(__file__))
